7ca18afb6e5bdd017598e53431966fc58a1c7e4b
Artmen_V <viliutich.artem@gmail.com>
2019-08-31 09:51:56 +0700

    push_pickup_savings_component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Row, Container } from "react-bootstrap";
 
-function App() {
-  return (
-    <Container className="container">
-      <Row className="purchase-card">
-        <h1>Hello React</h1>
-      </Row>
-    </Container>
-  );
+import { Subtotal } from "./components/subtotal/subtotal.js";
+import { PickupSavings } from "./components/PickupSaving/pickup-savings.js";
+
+class App extends Component {
+  state = {
+    total: 100,
+    PickupSavings: -3.85
+  };
+  render() {
+    return (
+      <Container className="container">
+        <Row className="purchase-card">
+          <Subtotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
